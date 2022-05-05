2b951c2c3c2f8bc5a3ecb96c813a5e27cf4b52ea
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-05-05 17:32:54 +0200

    Aggiornamento(singolo prodotto 1 (static)

--- resources/views/guest/detail.blade.php
+++ resources/views/guest/detail.blade.php
@@ -6,32 +6,70 @@
     <div class="jumbotron"></div>
     <div class="blue-container"></div>
 
-    <div class="container">
-        <div class="main-single-product-top">
-            <img class="product-poster" src="{{$comics[0]['thumb']}}" alt="$">
 
-            <div class="left-content">
-                <h2>Title</h2>
-                <div class="availability">
-                    <div class="av-left">sd</div>
-                    <div class="av-right">sdf</div>
-                </div>
-                <p>{{$comics[0]['description']}}</p>
+    <div class="main-single-product-top single-product-container">
+        <img class="product-poster" src="{{$comics[0]['thumb']}}" alt="$">
 
+        <div class="left-content">
+            <h2>Title</h2>
+            <div class="availability">
+                <div class="av-left">sd</div>
+                <div class="av-right">sdf</div>
             </div>
+            <p>{{$comics[0]['description']}}</p>
 
-            <div class="right-content">
-                <p>text</p>
-                <img src="/images/adv.jpg" alt="image">
-            </div>
+        </div>
+
+        <div class="right-content">
+            <p>text</p>
+            <img src="/images/adv.jpg" alt="image">
         </div>
     </div>
 
     
+    <section class="bg-product-bottom">
+        <div class="main-single-product-bottom single-product-container">
+            <div class="about-single-product">
+                <div class="talent">
 
-    <div class="main-single-product-bottom">
-        <div class="about-single-product"></div>
-        <div class="main-single-product-links"></div>
-    </div>
+                    <h3>Talent</h3>
+
+                    <div class="artists">
+                        <p class="display-inline single-product-title">Art by:</p>
+                        <p class="display-inline">kdfhks</p>
+                        
+                    </div>
+
+                    <div class="writers">
+                        <p class="display-inline single-product-title">Written by:</p>
+                        <p class="display-inline">safasf</p>
+                    </div>
+
+                </div>
+
+                <div class="specs">
+                    <h3>Specs</h3>
+
+                    <div class="series">
+                        <p class="display-inline single-product-title">Series:</p>
+                        <p class="display-inline">safasf</p>
+                    </div>
+
+                    <div class="specs-price">
+                        <p class="display-inline single-product-title">U.S. Price:</p>
+                        <p class="display-inline">safasf</p>
+                    </div>
+
+                    <div class="sale-date">
+                        <p class="display-inline single-product-title">On Sale Date:</p>
+                        <p class="display-inline">safasf</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="main-single-product-links"></div>
+        </div>
+    </section>
+    
 
 @endsection
